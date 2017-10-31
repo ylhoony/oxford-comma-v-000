0bd294d5eee9a695d8e043740f488fd7e3b98f5c
Hoon Lee <yl.hoony@gmail.com>
2017-10-31 08:04:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,18 @@
+# def oxford_comma(array)
+#   case array.length
+#     when 1 then array[0]
+#     when 2 then array.join(" and ")
+#     else
+#       a = [array.slice(0, array.length - 1), "and #{array.last}"]
+#       a.join(", ")
+#   end
+# end
+
 def oxford_comma(array)
-  case array.length
-    when 1 then array[0]
-    when 2 then array.join(" and ")
-    else
-      a = [array.slice(0, array.length - 1), "and #{array.last}"]
-      a.join(", ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")    
 end
