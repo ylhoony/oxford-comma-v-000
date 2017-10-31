e5cb473dc2d9d8b2c96efee2e85df543cd2a672d
Hoon Lee <yl.hoony@gmail.com>
2017-10-31 08:00:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1 then array[0]
+    when 2 then array.join(" and ")
+    else
+      a = [array.slice(0, array.length - 1), "and #{array.last}"]
+      a.join(", ")
+  end
 end
